5cfc1fec63d2dd5ed189a0e653a56bdb6a67d9f0
ghackford <hackford.j@northeastern.edu>
2022-02-28 23:54:24 -0500

    stripping down test to basic

--- src/tests/tuits-service.test.js
+++ src/tests/tuits-service.test.js
@@ -3,11 +3,9 @@ import {
   deleteTuit, findAllTuits,
   findTuitById, findTuitByUser
 } from "../services/tuits-service";
-import {createUser, deleteUsersByUsername} from "../services/users-service";
-
 
 describe('can create tuit with REST API', () => {
-  const aliceTuit = {
+  /**const aliceTuit = {
     tuit: "this is a test tuit",
     user: "6209970291a4560bc63f1e0a"
   };
@@ -17,6 +15,7 @@ describe('can create tuit with REST API', () => {
     expect(newTuit.tuit).toEqual("this is a test tuit");
     expect(newTuit.user).toEqual("6209970291a4560bc63f1e0a");
   });
+  */
 });
 
 describe('can delete tuit wtih REST API', () => {
@@ -25,6 +24,9 @@ describe('can delete tuit wtih REST API', () => {
 
 describe('can retrieve a tuit by their primary key with REST API', () => {
   // TODO: implement this
+  test('can retrieve tuits by user', async () => {
+    const existingTuits = await findTuitByUser("6209970291a4560bc63f1e0a")
+  })
 });
 
 describe('can retrieve all tuits with REST API', () => {
