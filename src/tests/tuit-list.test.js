46d2827017df4f52f800b2678b1f921f54f20cea
ghackford <hackford.j@northeastern.edu>
2022-03-10 00:27:18 -0500

    fleshing out test cases for tuit-list

--- src/tests/tuit-list.test.js
+++ src/tests/tuit-list.test.js
@@ -1,4 +1,4 @@
-import {Tuits} from "../components/tuits";
+import Tuit from "../components/tuits";
 import {screen, render} from "@testing-library/react";
 import {HashRouter} from "react-router-dom";
 import {findAllTuits} from "../services/tuits-service";
@@ -6,22 +6,37 @@ import axios from "axios";
 
 jest.mock('axios');
 
-const MOCKED_USERS = [
-  "alice", "bob", "charlie"
-];
-
+const tuitTesterA = "6229842538f323b59cbcb994"
+const tuitTesterB = "6229843638f323b59cbcb996"
+const tuitA = "6229890938f323b59cbcb9a4"
+const tuitB = "62298a2138f323b59cbcb9a6"
 const MOCKED_TUITS = [
-  "alice's tuit", "bob's tuit", "charlie's tuit"
+  {_id: tuitA, tuit: "testerA's tuit", postedBy: tuitTesterA, postedOn: "2022-03-10T05:13:45.178+00:00"},
+  {_id: tuitB, tuit: "testerB's tuit", postedBy: tuitTesterB, postedOn: "2022-03-10T05:18:25.967+00:00"}
 ];
 
 test('tuit list renders static tuit array', () => {
-  // TODO: implement this
+  render(
+    <HashRouter>
+      <Tuit tuits={MOCKED_TUITS}/>
+    </HashRouter>);
+  const linkElement = screen.getByText(/testerA/i);
+  expect(linkElement).toBeInTheDocument();
 });
 
-test('tuit list renders async', async () => {
-  // TODO: implement this
-})
+// tuit list renders async test in /src/tests/tuit-list-mock.test.js
 
 test('tuit list renders mocked', async () => {
-  // TODO: implement this
+  axios.get.mockImplementation(() =>
+    Promise.resolve({data: {tuits: MOCKED_TUITS} }));
+  const response = await findAllTuits();
+  const tuits = response.tuits;
+
+  render(
+    <HashRouter>
+      <Tuit tuits={tuits}/>
+    </HashRouter>);
+  
+  const tuit = screen.getByText(/testerB/i);
+  expect(tuit).toBeInTheDocument();
 });
